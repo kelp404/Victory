223eeaf8173b1cf86c8b9e370a76917acd30d7b6
Kelp <kelp@phate.org>
2013-03-15 16:46:58 +0800

    clean up

--- application/__init__.py
+++ application/__init__.py
@@ -7,45 +7,44 @@ app = Flask(__name__)
 app.config.from_object('application.config')
 
 
-# decompress gzip, gae developer tool do not support gzip
-class WSGICopyBody(object):
-    def __init__(self, application):
-        self.application = application
-
-    def __call__(self, environ, start_response):
-        content_encoding = environ.get('HTTP_CONTENT_ENCODING')
-        if content_encoding and content_encoding.find('gzip') >= 0:
-            del environ['HTTP_CONTENT_ENCODING']
-            # request body is gzip data
-            import gzip, StringIO
-
-            length = environ.get('CONTENT_LENGTH', '0')
-            length = 0 if length == '' else int(length)
-            body = environ['wsgi.input'].read(length)
-
-            # un gzip
-            gzipped = StringIO.StringIO(body)
-            reader = gzip.GzipFile(fileobj=gzipped, mode='rb')
-            new_content = reader.read()
-
-            environ['CONTENT_LENGTH'] = str(len(new_content))
-            environ['wsgi.input'] = StringIO.StringIO(new_content)
-
-        # Call the wrapped application
-        app_iter = self.application(environ, self._sr_callback(start_response))
-
-        # Return modified response
-        return app_iter
-
-    def _sr_callback(self, start_response):
-        def callback(status, headers, exc_info=None):
-
-            # Call upstream start_response
-            start_response(status, headers, exc_info)
-        return callback
-
 if config.DEBUG:
-    app.wsgi_app = WSGICopyBody(app.wsgi_app)
+    # decompress gzip, gae developer tool do not support gzip
+    class WSGIUngzipBody(object):
+        def __init__(self, application):
+            self.application = application
+
+        def __call__(self, environ, start_response):
+            content_encoding = environ.get('HTTP_CONTENT_ENCODING')
+            if content_encoding and content_encoding.find('gzip') >= 0:
+                del environ['HTTP_CONTENT_ENCODING']
+                # request body is gzip data
+                import gzip, StringIO
+
+                length = environ.get('CONTENT_LENGTH', '0')
+                length = 0 if length == '' else int(length)
+                body = environ['wsgi.input'].read(length)
+
+                # un gzip
+                gzipped = StringIO.StringIO(body)
+                reader = gzip.GzipFile(fileobj=gzipped, mode='rb')
+                new_content = reader.read()
+
+                environ['CONTENT_LENGTH'] = str(len(new_content))
+                environ['wsgi.input'] = StringIO.StringIO(new_content)
+
+            # Call the wrapped application
+            app_iter = self.application(environ, self._sr_callback(start_response))
+
+            # Return modified response
+            return app_iter
+
+        def _sr_callback(self, start_response):
+            def callback(status, headers, exc_info=None):
+
+                # Call upstream start_response
+                start_response(status, headers, exc_info)
+            return callback
+    app.wsgi_app = WSGIUngzipBody(app.wsgi_app)
 
 
 # set up router
